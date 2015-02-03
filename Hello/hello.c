f1db064bbf94dc5db798f6c10f8aaf7587b1b5a3
Max <marlier.maxime@gmail.com>
2015-02-03 21:31:39 +0100

    [add] hello world, use to test makfile with multiple program

---
+++ Hello/hello.c
@@ -0,0 +1,18 @@
+	/*
+ ============================================================================
+ Name        :
+ Author      :
+ Version     :
+ Copyright   :
+ Description :
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+ int main(int argc, char const *argv[])
+ {
+ 	printf("Hello world!\n");
+ 	return 0;
+ }
