f4b144c96906f837b0970b1b7b3cb1b15254cb8c
Max <marlier.maxime@gmail.com>
2015-02-04 15:51:16 +0100

    [ref] cleanup

--- ErathosteneStatic/ErathosteneStatic.c
+++ ErathosteneStatic/ErathosteneStatic.c
@@ -2,7 +2,7 @@
  ============================================================================
  Name        : ErathosteneStatic.c
  Author      : Marlier M.
- Version     :
+ Version     : 
  Copyright   : 
  Description : 
  ============================================================================
@@ -16,7 +16,6 @@
 typedef short int bool;
 #define false 0
 #define true 1
-
 #define MAX 100000
 #define PPREM_SIZE 78498
 
@@ -31,8 +30,6 @@ void Erathostene(struct TListePrem* l);
 void AfficherPremiers(struct TListePrem* l);
 void DetruireTListePrem(struct TListePrem* l);
 
-/* TODO ... */
-
 int main(int argc, char* argv[]) {
 	struct TListePrem listePrem;
 	do {
@@ -44,7 +41,6 @@ int main(int argc, char* argv[]) {
 	} while (!Stop());
 	return EXIT_SUCCESS;
 }
-
 
 void InitialiserTListePrem(struct TListePrem* l){
 	l->nbrMax = MAX;
@@ -86,6 +82,3 @@ void DetruireTListePrem(struct TListePrem* l){
 	for (i = 0; i < PPREM_SIZE; ++i)
 		l->pPrem[i] = 0;
 }
-
-
-/* TODO ... */
