e4bdfebd5080295f76da7be6f87fdfd6f065f629
Max <marlier.maxime@gmail.com>
2015-02-04 14:54:03 +0100

    [add] first working version

--- ErathosteneStatic/ErathosteneStatic.c
+++ ErathosteneStatic/ErathosteneStatic.c
@@ -1,15 +1,16 @@
-	/*
+/*
  ============================================================================
- Name        :
- Author      :
+ Name        : ErathosteneStatic.c
+ Author      : Marlier M.
  Version     :
- Copyright   :
- Description :
+ Copyright   : 
+ Description : 
  ============================================================================
  */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include "../InOut/src/inout.h"
 
 typedef short int bool;
@@ -17,10 +18,11 @@ typedef short int bool;
 #define true 1
 
 #define MAX 100000
+#define PPREM_SIZE 78498
 
 struct TListePrem {
 	int nbrMax;
-	int pPrem[78498];
+	int pPrem[PPREM_SIZE];
 	int nbrPrem;
 };
 
@@ -43,20 +45,46 @@ int main(int argc, char* argv[]) {
 	return EXIT_SUCCESS;
 }
 
-void InitialiserTListePrem(struct TListePrem* l){
 
+void InitialiserTListePrem(struct TListePrem* l){
+	l->nbrMax = MAX;
+	l->pPrem[0] = 0;
+	l->nbrPrem=0;
 }
 
 void Erathostene(struct TListePrem* l){
+	bool bools[MAX]; 
+	int i, nb = 2;
+	double racine;
 
+	for (i = 0; i < MAX; ++i)
+		bools[i] = true;
+
+	bools[0] = false;
+	bools[1] = false;
+
+	racine = sqrt(l->nbrMax);
+
+	for ( nb = 2; nb < racine; nb++)
+		for (i = nb; i <= l->nbrMax/nb; i++)
+			bools[i*nb] = false;
+
+	for (i = 0; i < l->nbrMax; ++i)
+		if(bools[i] == true)
+			l->pPrem[l->nbrPrem++] = i;
 }
 
 void AfficherPremiers(struct TListePrem* l){
-
+	int i;
+	for (i = 0; i < l->nbrPrem; ++i)
+		printf("%d;", l->pPrem[i]);
+	printf("\n");
 }
 
 void DetruireTListePrem(struct TListePrem* l){
-
+	int i;
+	for (i = 0; i < PPREM_SIZE; ++i)
+		l->pPrem[i] = 0;
 }
 
 
