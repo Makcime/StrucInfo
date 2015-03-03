294b5ddf7d1cff4eae103f3521df52bd31e084fe
Makcime <marlier.maxime@gmail.com>
2015-03-03 15:38:33 +0100

    initial commit for this exercice

---
+++ List/src/list.c
@@ -0,0 +1,47 @@
+/*
+ * list.c
+ *
+ * 
+ * Author: mattens
+ */
+/* ------------------------------------------------------------------------- */
+#define __DEBUG 0
+/* ------------------------------------------------------------------------- */
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "list.h"
+/* ------------------------------------------------------------------------- */
+typedef int bool;
+#define false 0
+#define true 1
+/* ------------------------------------------------------------------------- */
+struct TList {
+	struct __TNodeList* _pEndNode;
+	size_t _nodeCount;
+};
+/* ------------------------------------------------------------------------- */
+typedef struct __TNodeList _TNode;
+typedef struct __TNodeList* _TPNode;
+struct __TNodeList {
+	struct __TNodeList * pPrevious;
+	struct __TNodeList * pNext;
+	TValueList value;
+};
+/* ------------------------------------------------------------------------- */
+/* Declaration of prototypes of utilities funtions                           */
+static _TPNode _CreateHeaderNode();
+static _TPNode _CreateNode(const TValueList* pv, const _TPNode _pPrevious,
+		const _TPNode _pNext);
+/* ------------------------------------------------------------------------- */
+/* Definition of funtions declared in list.h                                 */
+/* ...                                                                       */
+/* ...                                                                       */
+/* ...                                                                       */
+/* ------------------------------------------------------------------------- */
+/* Definition of utilities funtions                                          */
+/* ...                                                                       */
+/* ...                                                                       */
+/* ...                                                                       */
+/* ------------------------------------------------------------------------- */
+
