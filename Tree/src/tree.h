53c01c54146b8c55b4716703607b560101a6df8d
Makcime <marlier.maxime@gmail.com>
2015-05-12 14:02:35 +0200

    [add] prototypes definition

--- Tree/src/tree.h
+++ Tree/src/tree.h
@@ -21,6 +21,23 @@ struct __TNodeTree;
 typedef struct __TNodeTree* TIteratorTree;
 /* ------------------------------------------------------------------------- */
 /* TODO ...                                                                  */
+struct TTree* pTree CreateTree(TKeyOf __KeyOf, TComp __KeyCompare);
+TKeyTree GetKeyInTree(struct TTree* pTree, TIteratorTree it);
+TValueTree* GetPDataInTree(TIteratorTree it);
+TIteratorTree BeginOfTree(struct TTree* pTree);
+TIteratorTree EndOfTree(struct TTree* pTree);
+bool IsEmptyTree(struct TTree* pTree);
+int SizeOfTree(struct TTree* pTree);
+TIteratorTree NextInTree(TIteratorTree it);
+TIteratorTree PreviousInTree(TIteratorTree it);
+void InsertInTree(struct TTree* pTree, struct TPairPtrSize* data);
+TIteratorTree EraseInTree(struct TTree* pTree, TIteratorTree it);
+void EraseKeysInTree(struct TTree* pTree, TKeyTree k);
+TIteratorTree UpperBoundInTree(struct TTree* pTree, TKeyTree k);
+TIteratorTree LowerBoundInTree(struct TTree* pTree, TKeyTree k);
+int CountNbrOfKeys(struct TTree* pTree, TKeyTree k);
+void WalkTree(struct TTree* pTree, FCallbackOnValue cb);
+
 /* ------------------------------------------------------------------------- */
 void __ShowPrefixTree(struct TTree* pTree, FCallbackOnValue fShowValue,
 		int isCallOnLeave);
